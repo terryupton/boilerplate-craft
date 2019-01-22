d86bb2e9e3209c3efc18e1e6d0cbf5ad73047e3a
Terry Upton <terry@amasci.co.uk>
2019-01-22 18:18:14 +0000

    Update to base general config settings

--- config/general.php
+++ config/general.php
@@ -9,54 +9,76 @@
  */
 
 return [
-    // Global settings
+  // Global settings
     '*' => [
-        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+      // Default Week Start Day (0 = Sunday, 1 = Monday...)
         'defaultWeekStartDay' => 1,
-
-        // Enable CSRF Protection (recommended)
+      
+      // Enable CSRF Protection (recommended)
         'enableCsrfProtection' => true,
-
-        // Whether generated URLs should omit "index.php" and have trailing slashes
+      
+      // URL
         'omitScriptNameInUrls' => true,
-        'addTrailingSlashesToUrls'         => true,
-
-        // Control Panel trigger word
+        'addTrailingSlashesToUrls' => true,
+      
+      // Control Panel trigger word
         'cpTrigger' => 'cms',
-
-        // The secure key Craft will use for hashing and encrypting data
+      
+      // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
-
-        // Use Project Config File for best practice in data migration
+      
+      // Use Project Config File for best practice in data migration
         'useProjectConfigFile' => true,
-
-        // Set fuzzy search by default
+      
+      // Set fuzzy search by default
         'defaultSearchTermOptions' => array(
             'subLeft' => true,
             'subRight' => true,
         ),
-
+        
         'generateTransformsBeforePageLoad' => true,
+        'useEmailAsUsername' => true,
+        'timezone' => 'Europe/London',
+      
+      // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
+      //        'aliases' => [
+      //            '@basePath' => getenv('BASE_PATH'),
+      //            '@baseUrl' => getenv('BASE_URL'),
+      //        ],
+    
     ],
-
-    // Dev environment settings
-    'dev' => [
-        // Base site URL
+  
+  // Dev environment settings
+    'local' => [
         'siteUrl' => null,
-
-        // Dev Mode (see https://craftcms.com/support/dev-mode)
         'devMode' => true,
+        'isSystemLive' => true,
+        'allowUpdates' => true,
+        'allowAdminChanges' => true,
+        'enableTemplateCaching' => false,
+        'testToEmailAddress' => 'terry@amasci.co.uk',
     ],
-
-    // Staging environment settings
+  
+  // Staging environment settings
     'staging' => [
-        // Base site URL
         'siteUrl' => null,
+        'allowUpdates' => false,
+        'backupOnUpdate' => false,
+        'devMode' => false,
+        'allowAdminChanges' => false,
+        'isSystemLive' => false,
+        'enableTemplateCaching' => false,
+        'testToEmailAddress' => 'terry@amasci.co.uk',
     ],
-
-    // Production environment settings
+  
+  // Production environment settings
     'production' => [
-        // Base site URL
         'siteUrl' => null,
+        'allowUpdates' => false,
+        'backupOnUpdate' => false,
+        'enableTemplateCaching' => true,
+        'devMode' => false,
+        'allowAdminChanges' => false,
+        'isSystemLive' => true,
     ],
 ];
