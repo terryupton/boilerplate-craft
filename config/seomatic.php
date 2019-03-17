04b51cc808b5ebbff0d23115ad594af457ecfd13
Terry Upton <terry@amasci.co.uk>
2019-03-17 14:42:58 +0000

    Added default SEOmatic settings file

---
+++ config/seomatic.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '*' => [
+        // The public-facing name of the plugin
+        'pluginName' => 'SEO &amp; Social Media',
+
+        // The server environment, either `live`, `staging`, or `local`
+        'environment' => getEnv("SEOMATIC_ENVIROMENT"),
+    ]
+];
